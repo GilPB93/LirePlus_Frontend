a1a6ffe1a3abcacbe73b1aaf433762fc491b4c09
GilPB93 <g.p.braga@hotmail.com>
2025-08-12 12:08:02 +0200

    Set user_library page

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -11,7 +11,8 @@ export const allRoutes = [
     new Route("/about", "À propos", "/pages/about.html"),
     new Route("/how_to", "Comment ça marche ?", "/pages/how_to.html"),
     new Route("/account", "Mon Compte", "/pages/user/account.html"),
-    new Route("/userhome", "Page d'accueil", "/pages/user/user_home.html")
+    new Route("/userhome", "Page d'accueil", "/pages/user/user_home.html"),
+    new Route("/userlibrary", "Ma Bibliothèque", "/pages/user/user_library.html")
 
 ];
 
